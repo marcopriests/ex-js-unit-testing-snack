baa3750b22b62c908b31e563f16966bd1e167bb2
marcopriests <prtmrc98@gmail.com>
2025-09-22 15:38:50 +0200

    [UPD] snack 3 done

--- snack.test.js
+++ snack.test.js
@@ -19,3 +19,17 @@ test('La funzione createSlug restituisce una stringa in lowercase.', () => {
     expect(createSlug('Tavolo')).toBe('tavolo');
     expect(createSlug('ARMADIO')).toBe('armadio');
 });
+
+// 🏆 Snack 3
+// Creare un test che verifichi la seguente descrizione:
+// 👉 "La funzione average calcola la media aritmetica di un array di numeri."
+
+const average = arr => arr.reduce((acc, num) => {
+    return (acc + num);
+}) / arr.length;
+
+test('La funzione average calcola la media aritmetica di un array di numeri.', () => {
+    expect(average([2, 4, 6, 8, 10])).toBe(6);
+    expect(average([2, 4, 6])).toBe(4);
+    expect(average([1, 3, 5, 7, 9])).toBe(5);
+});
